21413cd1cdde177f9138ce294be50946373c4aac
jaimesclar <jaim3s.14@gmail.com>
2025-08-03 03:05:48 -0500

    New index and correction the main.js path

---
+++ main.js
@@ -0,0 +1,32 @@
+import { scene } from './src/scene/setupScene.js';
+import { camera } from './src/scene/setupCamera.js';
+import { renderer } from './src/scene/setupRenderer.js';
+import { setupLights } from './src/scene/setupLights.js';
+import { setupControls } from './src/controls/setupControls.js';
+import { setupHDRLoader } from './src/loaders/loadHDR.js';
+import { setupModelLoader } from './src/loaders/loadModel.js';
+import { setupColorChanger } from './src/interactions/changeColor.js';
+import { setupTextureChanger } from './src/interactions/changeTexture.js';
+import { setupMaterialChanger } from './src/interactions/changeMaterial.js';
+
+setupLights(scene);
+const controls = setupControls(camera, renderer);
+setupHDRLoader(scene, renderer);
+setupModelLoader(scene);
+
+setupColorChanger();
+setupTextureChanger();
+setupMaterialChanger(scene);
+
+function animate() {
+  requestAnimationFrame(animate);
+  controls.update();
+  renderer.render(scene, camera);
+}
+animate();
+
+window.addEventListener('resize', () => {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+});
